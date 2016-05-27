9bbfdff2a1857381f1ece85cdb28370f84c72f8e
gregory.melson <gregory.melson@247-inc.com>
2016-05-27 11:46:28 -0700

    Add code for FB challenge response and Message in body

--- src/main.go
+++ src/main.go
@@ -8,19 +8,53 @@ import (
 	"nl"
 )
 
+type Body struct {
+	Object string `json:"object"`
+	Entry  []struct {
+		ID        string `json:"id"`
+		Time      int64  `json:"time"`
+		Messaging []struct {
+			Sender struct {
+				ID string `json:"id"`
+			} `json:"sender"`
+			Recipient struct {
+				ID string `json:"id"`
+			} `json:"recipient"`
+			Timestamp int64 `json:"timestamp"`
+			Message   struct {
+				Mid  string `json:"mid"`
+				Seq  int    `json:"seq"`
+				Text string `json:"text"`
+			} `json:"message"`
+		} `json:"messaging"`
+	} `json:"entry"`
+}
+
 func ProcessRequest(w http.ResponseWriter, req *http.Request) {
 	q := req.FormValue("message")
 	var answer string
 
-	// Substitute words and get Intent with Entities
-	message := new(nl.Message)
-	_, q = message.ReplaceWords(q)
-	err := message.GetIntent(q)
-	if err == nil {
-		//Answer based on rules
-		a := new(nl.Answer)
-		a.Message = *message
-		err, answer = a.GetAnswer()
+	//Handle messenger challenge if GET otherwise handle message
+	if req.Method == "GET" {
+		if req.URL.Query().Get("hub.verify_token") == "<validation_token>" {
+			answer = req.URL.Query().Get("hub.challenge")
+		} else {
+			answer = "Error, wrong validation token"
+		}
+	} else {
+		//This is a POST so expect FB Message
+		req.Body.Read()
+		// Substitute words and get Intent with Entities
+		message := new(nl.Message)
+		_, q = message.ReplaceWords(q)
+		err := message.GetIntent(q)
+		if err == nil {
+			//Answer based on rules
+			a := new(nl.Answer)
+			a.Message = *message
+			err, answer = a.GetAnswer()
+		}
+
 	}
 
 	//Set for cross domain stuff
